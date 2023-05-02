ee9855330eb888d89664c1354963e786a2721329
SimoneScordato99 <scordato.simone@yahoo.com>
2023-05-02 15:54:27 +0200

    pushaaaaaaaaa

---
+++ src/components/AppHero.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <img src="https://picsum.photos/id/237/500/300" alt="cane">
+    </div>
+</template>
+
+<style scoped>
+    
+    
+    
+</style>
+
+<script>
+
+export default{
+    name: "AppHero",
+    data() {
+        return {
+            
+        }
+    }
+}
+
+</script>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h1>hello</h1>
+    </div>
+</template>
+
+<style scoped>
+    
+    
+    
+</style>
+
+<script>
+
+export default{
+    name: "AppTitle",
+    data() {
+        return {
+            
+        }
+    }
+}
+
+</script>
+    
